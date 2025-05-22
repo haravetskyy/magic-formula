9d300f0c9ff8c6bd6818522a2614eb5b2cb28d11
haravetskyy <yuraharavetskyy@gmail.com>
2025-05-22 13:42:18 +0200

    init: Initialize basic `Next.js` project. Remove all initial boilerplate. Add `shadcn/ui`

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Magic Formula",
+  description: "A small app for basic math operations",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
